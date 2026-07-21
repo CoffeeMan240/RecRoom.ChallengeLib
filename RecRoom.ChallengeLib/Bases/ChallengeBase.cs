1b3687fbc1c298c543ac340e3a3df109b63e2278
coffee240 <68038942+CoffeeMan240@users.noreply.github.com>
2026-07-20 21:04:30 -0600

    add IPC

--- RecRoom.ChallengeLib/Bases/ChallengeBase.cs
+++ RecRoom.ChallengeLib/Bases/ChallengeBase.cs
@@ -6,11 +6,13 @@ namespace RecRoom.ChallengeLib;
 public abstract class ChallengeBase : IChallenge
 {
     public abstract ChallengeTypes ChallengeType { get; }
+    public bool IgnorePreviousCompletions { get; set; }
 
     public virtual Dictionary<string, object> Serialize()
     {
         var dictionary = new Dictionary<string, object>();
         dictionary.Add("ct", ChallengeType);
+        dictionary.Add("ipc", IgnorePreviousCompletions);
         return dictionary;
     }
 }
